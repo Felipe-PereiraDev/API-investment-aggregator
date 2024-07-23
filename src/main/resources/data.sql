ffdf59d2e622ac3ddbb6530885e1e805f62be610
Felipe <felpera41@gmail.com>
2024-07-23 16:40:32 -0400

    add db h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+-- Inserir ações do Brasil
+INSERT INTO tb_stocks (stock_id, description) VALUES ('PETR3', 'Petrobras - Ações Ordinárias');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('PETR4', 'Petrobras - Ações Preferenciais');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('VALE3', 'Vale S.A.');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('ITUB4', 'Itaú Unibanco');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('BBAS3', 'Banco do Brasil');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('ABEV3', 'Ambev');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('MGLU3', 'Magazine Luiza');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('BBDC3', 'Bradesco');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('JBSS3', 'JBS S.A.');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('SUZB3', 'Suzano Papel e Celulose');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('B3SA3', 'B3 S.A.');
+
+-- Inserir ações do mundo
+INSERT INTO tb_stocks (stock_id, description) VALUES ('AAPL', 'Apple Inc.');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('MSFT', 'Microsoft Corporation');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('AMZN', 'Amazon.com, Inc.');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('GOOGL', 'Alphabet Inc.');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('META', 'Meta Platforms, Inc.');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('TSLA', 'Tesla, Inc.');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('NVDA', 'NVIDIA Corporation');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('NFLX', 'Netflix, Inc.');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('INTC', 'Intel Corporation');
+INSERT INTO tb_stocks (stock_id, description) VALUES ('IBM', 'International Business Machines Corporation');
